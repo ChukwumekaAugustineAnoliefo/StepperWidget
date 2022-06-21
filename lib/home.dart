f7a32193cd53eb6770ffd7f8d79991cf874b0a28
ChukwumekaAugustineAnoliefo <chuksanoliefo@gmail.com>
2022-06-21 22:15:46 +0100

    created home statetful widget

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeStatefulWNameState();
+}
+
+class _HomeStatefulWNameState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
-St
+import 'package:flutter/material.dart';
+import 'package:stepper_widget/home.dart';
+
+void main() => runApp(const Main());
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Stepper Widget',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFF16171D),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          // or from RGB
+
+          primary: const Color(0xFF16171D),
+          secondary: const Color(0xFF16171D),
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
